0fd0bf348f45ec280f4537db7371f95d9ab11b48
Larissa Silveira <lalissa97@hotmail.com>
2026-02-24 10:08:29 -0700

    Login page first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import Login from './components/Login.jsx';
+
+function App() {
+
+  return (
+    <div>
+      <Login />
+    </div>
+   
+  )
+}
+
+export default App
---
+++ src/components/Login.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Login.css";
+import { FaUser, FaLock } from "react-icons/fa";
+
+const Login = () => {
+    return (
+        <div className='wrapper'>
+            <form action="">
+                <h1>Login</h1>
+                <div className="input-box">
+                    <input type="text" placeholder='ID number' required />
+                    <FaUser className='icon'/>
+                </div>
+                 <div className="input-box">
+                    <input type="password" placeholder='Password' required />
+                    <FaLock className='icon' />
+                </div>
+
+                <button type="submit">Let's Play!</button>
+            </form>
+        </div>
+    )
+}
+
+export default Login;
